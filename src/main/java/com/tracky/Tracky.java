7c2511aec2b7034f0746749abca4e3b5b145d22c
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2022-07-04 21:14:58 -0400

    default UUID

--- src/main/java/com/tracky/Tracky.java
+++ src/main/java/com/tracky/Tracky.java
@@ -2,11 +2,25 @@ package com.tracky;
 
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Random;
+import java.util.UUID;
+
 @Mod("tracky")
 public class Tracky {
+	private static UUID defaultUUID;
+	
 	public Tracky() {
 //		FMLJavaModLoadingContext.get().getModEventBus();
 //		MinecraftForge.EVENT_BUS;
-		TrackyAccessor.getForcedChunks(null);
+		
+		defaultUUID = new UUID(
+				Tracky.class.toString().hashCode() * 9383064L,
+				new Random(TrackyAccessor.class.toString().hashCode() + 32874).nextLong() * 10623261L
+		);
+	}
+	
+	// a default UUID dependent upon the name which the class is shaded under
+	public static UUID getDefaultUUID() {
+		return defaultUUID;
 	}
 }
