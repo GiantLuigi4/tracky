1689de2ffc499b1f7839b176a8300b115f3c6b18
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2022-07-07 22:56:58 -0400

    client stuff

--- src/main/java/com/tracky/MixinPlugin.java
+++ src/main/java/com/tracky/MixinPlugin.java
@@ -44,73 +44,107 @@ public class MixinPlugin implements IMixinConfigPlugin {
 	
 	private static final String chunkPosClass = "net/minecraft/util/math/ChunkPos";
 	private static final String worldClass = "net/minecraft/world/World";
+	private static final String clientWorldClass = "net/minecraft/client/world/ClientWorld";
 	private static final String playerClass = "net/minecraft/entity/Player";
 	
 	FieldNode targetField;
+	FieldNode renderField;
 	
 	private static final String type = "java/util/Function<L" + playerClass + ";L" + chunkPosClass + ";>";
+	private static final String typeClient = "java/util/Supplier<L" + chunkPosClass + ";>";
+	private static final String typeServer = "java/util/Map<Ljava/util/UUID;L" + type + ";>";
+	
+	public FieldNode injectField(ClassNode targetClass, String fieldName, boolean isMainCheck, boolean isMap) {
+		for (FieldNode field : targetClass.fields) {
+			if (field.name.equals(fieldName)) {
+				if (isMainCheck)
+					isMainTracky = false;
+				targetField = field;
+				return field;
+			}
+		}
+		
+		FieldNode nd;
+		targetClass.fields.add(nd = new FieldNode(
+				// synthetic hides it from the decompiler
+				Opcodes.ACC_PUBLIC | Opcodes.ACC_SYNTHETIC,
+				fieldName,
+				"Ljava/util/Map;",
+				isMap ? "L" + typeServer + ";" : "L" + typeClient + ";",
+				null
+		));
+		isMainTracky = true;
+		for (MethodNode method : targetClass.methods) {
+			if (method.name.equals("<init>")) {
+				ArrayList<AbstractInsnNode> targets = new ArrayList<>();
+				for (AbstractInsnNode instruction : method.instructions) {
+					if (instruction.getOpcode() == Opcodes.RETURN) {
+						targets.add(instruction);
+					}
+				}
+				for (AbstractInsnNode target : targets) {
+					InsnList list = new InsnList();
+					String des = nd.desc.substring(1, nd.desc.length() - 1);
+					list.add(new VarInsnNode(Opcodes.ALOAD, 0));
+					list.add(new TypeInsnNode(Opcodes.NEW, des.replace("Map", "HashMap")));
+					list.add(new InsnNode(Opcodes.DUP));
+					list.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, des.replace("Map", "HashMap"), "<init>", "()V"));
+					AbstractInsnNode insn = new FieldInsnNode(Opcodes.PUTFIELD, targetClass.name.replace(".", "/"), nd.name, nd.desc);
+					list.add(insn);
+					method.instructions.insertBefore(target, list);
+				}
+			}
+		}
+		
+		dump(targetClass);
+		
+		return nd;
+	}
 	
 	@Override
 	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+		// so, the jvm really does not care about the name of a field
+		// so I can do stupid stuff like this, and have essentially a 0% chance of anyone ever happening to have the same field name
+		// however, this also guarantees that there is a 100% certainty that all trackies use the same central field
+		// this makes it possible to make it so that only the main tracky does stuff
 		String fieldName = trackyUUID.toString() + " Tracky Forced";
-		if (targetClassName.equals(worldClass.replace("/", "."))) {
-			// so, the jvm really does not care about the name of a field
-			// so I can do stupid stuff like this, and have essentially a 0% chance of anyone ever happening to have the same field name
-			for (FieldNode field : targetClass.fields) {
-				if (field.name.equals(fieldName)) {
-					isMainTracky = false;
-					targetField = field;
-					return;
-				}
-			}
-			
-			targetClass.fields.add(targetField = new FieldNode(
-					// synthetic hides it from the decompiler
-					Opcodes.ACC_PUBLIC | Opcodes.ACC_SYNTHETIC,
-					fieldName,
-					"Ljava/util/Map;",
-					"Ljava/util/Map<Ljava/util/UUID;L" + type + ";>;",
-					null
-			));
-			isMainTracky = true;
-			for (MethodNode method : targetClass.methods) {
-				if (method.name.equals("<init>")) {
-					ArrayList<AbstractInsnNode> targets = new ArrayList<>();
-					for (AbstractInsnNode instruction : method.instructions) {
-						if (instruction.getOpcode() == Opcodes.RETURN) {
-							targets.add(instruction);
-						}
-					}
-					for (AbstractInsnNode target : targets) {
-						InsnList list = new InsnList();
-						String des = targetField.desc.substring(1, targetField.desc.length() - 1);
-						list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-						list.add(new TypeInsnNode(Opcodes.NEW, des.replace("Map", "HashMap")));
-						list.add(new InsnNode(Opcodes.DUP));
-						list.add(new MethodInsnNode(Opcodes.INVOKESPECIAL, des.replace("Map", "HashMap"), "<init>", "()V"));
-						AbstractInsnNode insn = new FieldInsnNode(Opcodes.PUTFIELD, targetClassName.replace(".", "/"), targetField.name, targetField.desc);
-						list.add(insn);
-						method.instructions.insertBefore(target, list);
-					}
-				}
-			}
-			
-			dump(targetClass);
+		// idk why intelliJ complained about the "toString" on the one below but not the one above, but ok
+		String renderFieldName = trackyUUID + " Tracky Rendered";
+		if (targetClassName.equals(clientWorldClass.replace("/", "."))) {
+			renderField = injectField(targetClass, renderFieldName, false, false);
+		} else if (targetClassName.equals(worldClass.replace("/", "."))) {
+			targetField = injectField(targetClass, fieldName, true, true);
 		} else {
 			for (MethodNode method : targetClass.methods) {
-				if (method.name.equals("getForcedChunks")) {
+				FieldNode targ = null;
+				String owner = null;
+				switch (method.name) {
+					case "getForcedChunks": {
+						targ = targetField;
+						owner = worldClass;
+						break;
+					}
+					case "getRenderedChunks": {
+						targ = renderField;
+						owner = clientWorldClass;
+						break;
+					}
+					case "<init>":
+						method.access = method.access | Opcodes.ACC_SYNTHETIC;
+						break;
+				}
+				if (targ != null) {
 					String sig = method.signature.substring(method.signature.indexOf(")") + 1);
 					method.instructions.clear();
 					method.instructions.add(new VarInsnNode(Opcodes.ALOAD, 0));
 					String desc = sig.substring(0, sig.indexOf("<")) + ";";
+					method.instructions.add(new TypeInsnNode(Opcodes.CHECKCAST, owner));
 					method.instructions.add(new FieldInsnNode(
-							Opcodes.GETFIELD, worldClass,
-							fieldName, desc
+							Opcodes.GETFIELD, owner,
+							targ.name, desc
 					));
 					method.instructions.add(new InsnNode(Opcodes.ARETURN));
-					method.access = Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC | Opcodes.ACC_SYNTHETIC;
-				} else if (method.name.equals("<init>")) {
-					method.access = method.access | Opcodes.ACC_SYNTHETIC;
+					method.access = Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC;
 				}
 			}
 			
@@ -123,7 +157,7 @@ public class MixinPlugin implements IMixinConfigPlugin {
 		clazz.accept(writer);
 		try {
 			File f = new File("dmp/" + clazz.name + ".class");
-			if (!f.exists()) f.getParentFile().mkdirs();
+			if (!f.exists()) f.getParentFile().mkdirs(); // how do I suppress ignored result?
 			FileOutputStream outputStream = new FileOutputStream(f);
 			byte[] bytes = writer.toByteArray();
 			outputStream.write(bytes);
--- src/main/java/com/tracky/TrackyAccessor.java
+++ src/main/java/com/tracky/TrackyAccessor.java
@@ -7,6 +7,7 @@ import net.minecraft.world.World;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class TrackyAccessor {
 	/**
@@ -18,5 +19,14 @@ public class TrackyAccessor {
 	 * @return the list of force loaded chunks
 	 */
 	// also it's native so that it doesn't have a method body
-	public static native Map<UUID, Function<PlayerEntity, ChunkPos>> getForcedChunks(World world);
+	public static native Map<UUID, Function<PlayerEntity, Iterable<ChunkPos>>> getForcedChunks(World world);
+	
+	/**
+	 * client only
+	 * is a function as QOL
+	 *
+	 * having it be a supplier allows it to change multiple times per frame without
+	 * having the dev need to change any variables for every render
+	 */
+	public static native Supplier<Iterable<ChunkPos>> getRenderedChunks(World world);
 }
