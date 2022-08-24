ffc8d7a7a6260c39553d8511a1587fe4c1abca2f
GiantLuigi4 <49770992+GiantLuigi4@users.noreply.github.com>
2022-08-24 16:43:24 -0400

    i still have no idea what i'm doing

--- src/test/java/HashCodes.java
+++ src/test/java/HashCodes.java
@@ -1,17 +1,17 @@
-import com.tracky.MixinPlugin;
-
-public class HashCodes {
-	public static void main(String[] args) {
-		System.out.println(MixinPlugin.class.toString());
-		int code = MixinPlugin.class.toString().hashCode();
-		
-		String str = MixinPlugin.class.toString();
-		for (int i = 0; i < 203993; i++) {
-			String str1 = str.replace("com", "" + i);
-			int code1 = str1.hashCode();
-			if (code1 >code) {
-				System.out.println(str1);
-			}
-		}
-	}
-}
+//import com.tracky.MixinPlugin;
+//
+//public class HashCodes {
+//	public static void main(String[] args) {
+//		System.out.println(MixinPlugin.class.toString());
+//		int code = MixinPlugin.class.toString().hashCode();
+//
+//		String str = MixinPlugin.class.toString();
+//		for (int i = 0; i < 203993; i++) {
+//			String str1 = str.replace("com", "" + i);
+//			int code1 = str1.hashCode();
+//			if (code1 >code) {
+//				System.out.println(str1);
+//			}
+//		}
+//	}
+//}
