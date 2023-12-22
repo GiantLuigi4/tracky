316fb5565e86c1c9d30b1349803f7b288aeaf384
Ocelot <FinntheRaider@gmail.com>
2023-12-21 18:09:51 -0700

    Removed test code

--- src/main/java/com/tracky/mixin/dummy/LevelMixin.java
+++ src/main/java/com/tracky/mixin/dummy/LevelMixin.java
@@ -10,14 +10,11 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.WritableLevelData;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -28,8 +25,6 @@ import java.util.function.Supplier;
 
 @Mixin(value = Level.class, priority = 500)
 public abstract class LevelMixin implements ServerMapHolder {
-
-	@Shadow public abstract long getDayTime();
 
 	@Unique
 	private Map<UUID, Supplier<Collection<TrackingSource>>> tracky$TrackingSources;
@@ -47,26 +42,13 @@ public abstract class LevelMixin implements ServerMapHolder {
 				new ChunkPos(new BlockPos(-656 * 1, -63, 656 * 1)),
 				new ChunkPos(new BlockPos(656 * 1, 319, 656 * 2))
 		);
-		boolean[] shown = new boolean[1];
-		shown[0] = true;
 
 		TrackyAccessor.getTrackingSources(((Level) (Object) this)).put(
 				Tracky.getDefaultUUID("tracky", "testing"),
 				() -> {
 					if (Tracky.ENABLE_TEST) {
-						if (getDayTime() % 200 == 0) {
-							if ((Object) this instanceof ServerLevel svrlvl) {
-								shown[0] = !shown[0];
-								for (ServerPlayer player : svrlvl.getPlayers((p) -> true)) {
-									TrackyAccessor.markForRetracking(player);
-								}
-							}
-						}
-
-						if (!shown[0]) return List.of();
 						return List.of(source);
 					}
-
 					return List.of();
 				}
 		);
